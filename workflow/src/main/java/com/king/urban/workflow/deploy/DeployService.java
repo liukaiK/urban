a4d2e0d4d71ab849023eb76ea7a9c00ff3002ed6
liukai <zhizhufan@foxmail.com>
2023-06-15 13:43:03 +0800

    DeployController

--- workflow/src/main/java/com/king/urban/workflow/deploy/DeployService.java
+++
@@ -1,7 +0,0 @@
-package com.king.urban.workflow.deploy;
-
-public interface DeployService {
-
-    void deploy(String name, String xml);
-
-}
---
+++ workflow/src/main/java/com/king/urban/workflow/service/DeployService.java
@@ -0,0 +1,7 @@
+package com.king.urban.workflow.service;
+
+public interface DeployService {
+
+    void deploy(String resourceName, String xml);
+
+}
