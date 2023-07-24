feedf106407059422bfebb88f06a326d36489d71
liukai <zhizhufan@foxmail.com>
2023-07-24 09:00:37 +0800

    CORS配置

--- security/src/main/java/com/king/urban/security/web/authentication/WebAuthenticationFailureHandler.java
+++ security/src/main/java/com/king/urban/security/web/authentication/WebAuthenticationFailureHandler.java
@@ -1,5 +1,10 @@
 package com.king.urban.security.web.authentication;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.king.urban.common.Result;
+import com.king.urban.common.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -12,9 +17,12 @@ import java.io.IOException;
 @Component
 public class WebAuthenticationFailureHandler implements AuthenticationFailureHandler {
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-
+        ResponseUtil.write(response, objectMapper.writeValueAsString(Result.fail("1111", "账号或密码错误")), MediaType.APPLICATION_JSON_VALUE);
     }
 
 }
