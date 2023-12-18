151c8c1c145514aeca6543df67302715ec067c65
liukai <zhizhufan@foxmail.com>
2023-12-18 16:25:25 +0800

    update

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,14 +1,31 @@
 import $ from 'jquery';
 import './style.css';
 
-const login_btn_id = "login_btn";
+// import {
+//     Check,
+//     Delete,
+//     Edit,
+//     Message,
+//     Search,
+//     Star,
+// } from 'element-plus';
+
+
 const login_url = 'http://127.0.0.1:8888/login';
 
 window.onload = function () {
-    document.getElementById(login_btn_id).onclick = function () {
+    document.querySelector('#login_btn').addEventListener('click', function () {
         login();
-    };
+    }, false);
 };
+
+// createApp({
+//     data() {
+//         return {
+//             username: 'liukai'
+//         }
+//     }
+// }).mount('#app');
 
 function login() {
     let username = $('#username').val();
